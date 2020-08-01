ea98b57b8657bd2397d26dfc5ea03be1c2f402a6
Seth Purr <eric.purrington@gmail.com>
2020-07-31 18:48:43 -0700

    close to working api routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,27 +1,41 @@
 import axios from "axios";
-import "dotenv";
-const BASEURL = "https://www.googleapis.com/civicinfo/v2";
-const APIKEY = process.env.REACT_APP_GAPI_KEY;
 
 export default {
     // Get all live upcoming elections
     // Doesn't req zip or address
-    getElections: function () {
-        return axios.get(BASEURL + "/elections?key=" + APIKEY);
+    // getElections: function () {
+    //     return axios.get(BASEURL + "/elections?key=" + APIKEY);
+    // },
+    getElections: function() {
+        return axios.get("/api/getElections");
     },
     // Get voters info (elections they can vote in and ballot drop off location's address and name)
     // Requires full address and only works if they have elections coming up
     // Address must be given with %20 in place of spaces and no punctuation
-    getVoterInfo: function (address) {
-        return axios.get(BASEURL + "/voterinfo?key=" + APIKEY + "&address=" + address);
+    // getVoterInfo: function (location) {
+    //     return axios.get(BASEURL + "/voterinfo?key=" + APIKEY + "&address=" + address);
+    // },
+    getVoterInfo: function(location) {
+        return axios.get("/api/getVoterInfo", {
+            params: {
+                location: location
+            }
+        });
     },
     // Get all reps for voter
     // Required zipcode and optional address for more results
-    getRepInfo: function (zip) {
-        return axios.get(BASEURL + "/representatives?key=" + APIKEY + "&address=" + zip);
+    // getRepInfo: function (location) {
+    //     return axios.get(BASEURL + "/representatives?key=" + APIKEY + "&address=" + zip);
+    // },
+    getRepInfo: function(location) {
+        return axios.get("/api/getRepInfo", {
+            params: {
+                location: location
+            }
+        });
     },
     // Get signed in user
     getUser: function (id) {
-        return axios.get("/" + id);
+        return axios.get("/api/users/" + id);
     }
 }
---
+++ routes/api/gapi.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+require("dotenv").config();
+const axios = require("axios");
+const BASEURL = "https://www.googleapis.com/civicinfo/v2";
+const APIKEY = process.env.REACT_APP_GAPI_KEY;
+
+// Should match /api/getElections
+router.get("/getElections", (req, res) => {
+    axios.get(BASEURL + "/elections?key=" + APIKEY)
+    .then(response => res.send(response.data))
+    .catch(err => console.log(err));
+});
+
+// Should match /api/getVoterInfo
+router.get("/getVoterInfo", (req, res) => {
+    axios.get(BASEURL + "/voterinfo?key=" + APIKEY + "&address=" + req.query.location)
+    .then(response => res.send(response.data))
+    .catch(err => console.log(err));
+});
+
+// Should match /api/getRepInfo
+router.get("/getRepInfo", (req, res) => {
+    axios.get(BASEURL + "/representatives?key=" + APIKEY + "&address=" + req.query.location)
+    .then(response => res.send(response.data))
+    .catch(err => console.log(err));
+});
+
+module.exports = router;
