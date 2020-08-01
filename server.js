153e1416414aced9f7b80aa1e561c7f01257a798
Seth Purr <eric.purrington@gmail.com>
2020-08-01 12:58:16 -0700

    api now pulls from backend

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -24,7 +23,7 @@ mongoose.connect(
 );
 
 // // Use apiRoutes
-app.use("/api", routes);
+// app.use("/api", routes);
 
 // // Send every request to the React app
 // // Define any API routes before this runs
