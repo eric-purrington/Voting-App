919d0e3f5ab070f89205e0fb175708d7160442b6
Joanna Preston <joannappreston@gmail.com>
2020-08-04 21:29:17 -0700

    changed user schema

--- routes/api/users.js
+++ routes/api/users.js
@@ -13,4 +13,14 @@ router
   .put(usersController.update)
   .delete(usersController.remove);
 
+// Matches with "/api/users/officials/:id"
+router
+  .route("/officials/:id")
+  .put(usersController.addOfficial);
+
+// Matches with "/api/users/events/:id"
+router
+  .route("/events/:id")
+  .put(usersController.addEvent);
+
 module.exports = router;
