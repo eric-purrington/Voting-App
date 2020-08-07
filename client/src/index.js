679fb7e3ce37dc7ba64135a39a4d9ad1773d91e1
alyssahellrung <alyssahellrung@gmail.com>
2020-08-06 21:29:49 -0700

    adding more auth0, fixing an error in a file name

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import Auth0ProviderWithHistory from "./auth0-provider-wtih-history";
+import Auth0ProviderWithHistory from "./auth0-provider-with-history";
 import "./index.css";
 
 ReactDOM.render(
