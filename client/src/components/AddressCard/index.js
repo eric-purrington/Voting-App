4625e44fc8f9d8c6d8af513cd69558d3cab0c05e
Sabah Syed <sabisyed@gmail.com>
2020-08-08 13:51:32 -0700

    Hover func added to cards

--- client/src/components/AddressCard/index.js
+++ client/src/components/AddressCard/index.js
@@ -1,20 +1,26 @@
 import React from "react";
 import UserAPI from "../../utils/UserAPI"
 import "./style.css";
-
+import PollingInfo from "../PollingInfo"
 function AddressCard(props) {
 
     return (
-        <div className = "addressCardContainer">
-            <div id={props.line1.slice(0, 3)} className="card">
-            <div class="uk-animation-toggle" tabindex="0">
-                <div class="uk-animation-slide-left">
-                <h5>Location: {props.locationName}</h5>
-                <h6>Address: {props.line1}</h6>                
-                <h6> {props.city} {props.state} {props.zip}</h6>
+        <div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
+            <div>
+                <div class="uk-card uk-card-hover uk-card-body">
+                    <h3 class="uk-card-title">Address</h3>
+                    <div className="addressCardContainer">
+                        <div id={props.line1.slice(0, 3)} className="card">
+                            <div class="uk-animation-toggle" tabindex="0">
+                                <div class="uk-animation-slide-left">
+                                    <h5> {props.locationName}</h5>
+                                    <h6>Address: {props.line1},{props.city} ,{props.state}, {props.zip}</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                </div>
-                </div>
+            </div>
         </div>
     )
 };
--- client/src/components/PollingInfo/index.js
+++ client/src/components/PollingInfo/index.js
@@ -2,21 +2,29 @@ import React from "react";
 import "./style.css";
 
 function PollingInfo(props) {
-  return (
-    <div className = "PollingInfoContainer">
-        <div id={props.name.slice(0, 1)} className="card">
-            <div class="uk-animation-toggle" tabindex="0">
-                <div class="uk-animation-slide-left">
-                    <p>Election name: {props.name}</p>
-                    <p>Election Info Url: {props.electionInfoUrl}</p>
-                    <p>Ballot Url : {props.ballotInfoUrl}</p>                
-                    <p> Registration Url: {props.electionRegistrationUrl}</p>
-                    <p> Registration Confirmation Url: {props.electionRegistrationConfirmationUrl}</p>
+    return (
+
+        <div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
+            <div>
+                <div class="uk-card uk-card-hover uk-card-body">
+                    <h3 class="uk-card-title">Election Info</h3>
+                    <div className="PollingInfoContainer">
+                        <div id={props.name.slice(0, 3)} className="card">
+                            <div class="uk-animation-toggle" tabindex="0">
+                                <div class="uk-animation-slide-left">
+                                    <p>Election name: {props.name}</p>
+                                    <p>Election Info Url: {props.electionInfoUrl}</p>
+                                    <p>Ballot Url : {props.ballotInfoUrl}</p>
+                                    <p> Registration Url: {props.electionRegistrationUrl}</p>
+                                    <p> Registration Confirmation Url: {props.electionRegistrationConfirmationUrl}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-</div>
-  );
+    );
 }
 
 export default PollingInfo;
