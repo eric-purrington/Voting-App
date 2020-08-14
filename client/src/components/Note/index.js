6034139ba68bea202ff20f99330677c600f25f5a
Joanna Preston <joannappreston@gmail.com>
2020-08-13 21:23:08 -0700

    added event added modal

--- client/src/components/Note/index.js
+++ client/src/components/Note/index.js
@@ -3,10 +3,8 @@ import "./style.css";
 
 function Note() {
   return (
-    <div className="uk-alert-warning" uk-alert>
-        {/* <a className="uk-alert-close" uk-close="true"></a> */}
-        {/* <h4 className = "h4">Note :</h4> */}
-        <p> If you do not see any results for your address, please visit the <a href ="https://www.vote.org/polling-place-locator/" target="_blank" rel="noopener noreferrer">Polling Place Locater</a> for more details.</p>
+    <div className="uk-alert-warning" uk-alert="true">
+      <p> If you do not see any results for your address, please visit the <a href="https://www.vote.org/polling-place-locator/" target="_blank" rel="noopener noreferrer">Polling Place Locater</a> for more details.</p>
     </div>
   );
 }
